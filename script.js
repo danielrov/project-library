879495997546916bea1e76b53eb95a97ee3afb49
danielrov <80066620+danielrov@users.noreply.github.com>
2024-08-06 10:00:19 -0600

    Class instead of plain constructor

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const myLibrary = [];
 let bookNumber = 0;
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-};
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
   
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title,author,pages,read);
